3713132a36cb564e8b0817b6b9c38f42bf79833f
tanyiqu <1953649096@qq.com>
2021-10-24 17:47:02 +0800

    实现Example组件

--- ui/Widgets/Example/ExampleWidget.py
+++ ui/Widgets/Example/ExampleWidget.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QWidget
 from ui.Widgets.Example.uic_ExampleWidget import Ui_ExampleWidget
+import ui.Widgets.Example.example as example
 
 
 class ExampleWidget(QWidget):
@@ -11,18 +12,29 @@ class ExampleWidget(QWidget):
         self.initFunc()
         pass
 
-    # 初始化
+        # 初始化
+
     def initFunc(self):
         self.widget.btn_encode.clicked.connect(self.encode)
         self.widget.btn_decode.clicked.connect(self.decode)
         pass
 
     def encode(self):
-        print('encode')
+        # print('encode')
+        # 获取文本框中的内容
+        txt_left = self.widget.txt_left.toPlainText()
+        txt_right = example.encode(txt_left)
+        print(txt_right)
+        self.widget.txt_right.setPlainText(txt_right)
         pass
 
     def decode(self):
         print('decode')
+        # 获取文本框中的内容
+        txt_left = self.widget.txt_left.toPlainText()
+        txt_right = example.decode(txt_left)
+        print(txt_right)
+        self.widget.txt_right.setPlainText(txt_right)
         pass
 
     pass
